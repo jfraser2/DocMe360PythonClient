3e14efc18c61b4e1f73e53331cf2fb741b404411
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-08-14 18:12:01 -0500

    added the tabbed panel to the display

--- src/app.py
+++ src/app.py
@@ -6,13 +6,14 @@ Created on Aug 14, 2025
 
 if __name__ == '__main__':
     from panels.main import MainPanel
+    from tkinter import ttk
     import tkinter as tk
 
     root = tk.Tk()  # This creates the main window instance.
     root.title("DocMe360 GUI App") # Sets the window title.
     root.geometry("400x300") # Sets the initial size of the window.  
   
-    mainPanel = MainPanel(root, tk)
+    mainPanel = MainPanel(root, tk, ttk)
     mainPanel.build()
   
     # Start the Event Loop
--- src/panels/main.py
+++ src/panels/main.py
@@ -12,36 +12,56 @@ class MainPanel(object):
         classdocs
     '''
     
-    def __init__(self, appRoot, tk):
+    def __init__(self, appRoot, tk, ttk):
         self.appRoot = appRoot
         self.tk = tk
+        self.ttk = ttk
         
     def on_exit_click(self):
         self.system.exit() 
            
     def build(self) :   
         # Configure row and column weights for expansion
-        self.appRoot.grid_rowconfigure(0, weight=3) # Make the first Row Taller
+        self.appRoot.grid_rowconfigure(0, weight=5) # Make the first Row Taller
         self.appRoot.grid_rowconfigure(1, weight=1)
         self.appRoot.grid_columnconfigure(0, weight=1)
-        self.appRoot.grid_columnconfigure(1, weight=3) # Make second column wider
+        self.appRoot.grid_columnconfigure(1, weight=4) # Make second column wider
         
         # Create a Frame to act as a "panel"
         self.east_panel = self.tk.Frame(self.appRoot, bg="lightblue")
         # Sticky "nsew" makes it fill the cell
-        self.east_panel.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+        self.east_panel.grid(row=0, column=0, sticky="nsew", padx=5, pady=5)
      
-        # Create a Frame to act as a "panel"
-        self.west_panel = self.tk.Frame(self.appRoot, bg="lightblue")
-        # Sticky "nsew" makes it fill the cell
-        self.west_panel.grid(row=0, column=1, sticky="nsew", padx=10, pady=10)
+        # Create a Notebook to act as a "tabbed panel"
+        self.buildTabbedPanel()
+        # Place the Notebook in the west Panel
+        self.notebook.grid(row=0, column=1, sticky="nsew", padx=5, pady=5)
     
         # Create a Frame to act as a "panel"
         self.south_panel = self.tk.Frame(self.appRoot, bg="white")
         # Sticky "nsew" makes it fill the cell
-        self.south_panel.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=10, pady=10)
+        self.south_panel.grid(row=1, column=0, columnspan=2, sticky="nsew", padx=5, pady=5)
     
     
         self.exit_button = self.tk.Button(self.south_panel, text="Exit", command=self.on_exit_click)
         self.exit_button.place(relx=0.5, rely=0.5, anchor=self.tk.CENTER) # Center the button
+        
+    def buildTabbedPanel(self) :
+        
+        self.notebook = self.ttk.Notebook(self.appRoot)
+
+        # Create individual tab frames
+        tab1 = self.ttk.Frame(self.notebook)
+        tab2 = self.ttk.Frame(self.notebook)
+
+        # Add Button to tab1
+        self.tk.Button(tab1, text="Notifications")
+
+        # Add Button to tab2
+        self.tk.Button(tab2, text="Templates")
+
+        # Add tabs to the Notebook
+        self.notebook.add(tab1, text="Notifications")
+        self.notebook.add(tab2, text="Templates")
+                 
     
