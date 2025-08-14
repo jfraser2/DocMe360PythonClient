89edeb56fe4fa8723a3a39ae843b5bc5d6551484
jfraser2 <69680391+jfraser2@users.noreply.github.com>
2025-08-14 14:36:46 -0500

    completed app skeleton

--- src/app.py
+++ src/app.py
@@ -5,4 +5,17 @@ Created on Aug 14, 2025
 '''
 
 if __name__ == '__main__':
-    pass
+  from panels.main import MainPanel
+  import tkinter as tk
+
+  root = tk.Tk()  # This creates the main window instance.
+  root.title("DocMe360 GUI App") # Sets the window title.
+  root.geometry("400x300") # Sets the initial size of the window.  
+  
+  mainPanel = MainPanel(root, tk)
+  mainPanel.build()
+  
+  # Start the Event Loop
+  root.mainloop()
+      
+  pass
